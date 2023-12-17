4d87b18e1788eb4a3e91dfe9220ab804fc6b1569
Rimuru <Rimuru.Dev@gmail.com>
2023-12-17 20:57:53 +0400

    Create PriverAddMoney.ts

---
+++ Course/Introduction/PriverAddMoney.ts
@@ -0,0 +1,20 @@
+﻿interface IUser
+{
+    Name: string;
+    Money: number;
+}
+
+function AddMoney(user: IUser, sum: number): IUser
+{
+    if (sum < 0)
+    {
+        console.error("Error");
+        return user;
+    }
+
+    user.Money += sum;
+
+    return user;
+}
+
+AddMoney({Name: 'Frost', Money: 10}, 5)
